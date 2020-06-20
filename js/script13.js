be507247ff846a169e5937eb716b5bea10e82713
Leo Porto <leonardoreisporto@gmail.com>
2020-06-20 07:16:36 -0300

    Initial commit

---
+++ js/script13.js
@@ -0,0 +1,46 @@
+let n1 = document.querySelector('#n1')
+let n2 = document.querySelector('#n2')
+let n3 = document.querySelector('#n3')
+let certo = document.querySelector('#certo')
+
+
+n1.onclick = function () {
+
+    n1.classList.remove('text-light')
+
+    n1.classList.add('errado')
+    
+}
+
+
+n2.onclick = function () {
+
+    n2.classList.remove('text-light')
+
+    n2.classList.add('errado')
+    
+}
+
+
+n3.onclick = function () {
+
+    n3.classList.remove('text-light')
+
+    n3.classList.add('errado')
+    
+}
+
+
+certo.onclick = function () {
+
+    certo.classList.remove('text-light')
+
+    certo.classList.add('certo')
+
+    setTimeout(() => {
+
+        window.location = 'q14.html'
+        
+    }, 1000);
+    
+}
